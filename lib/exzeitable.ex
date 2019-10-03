98ceb120014937b96187d0659de5e2932f8ab862
Alan Vardy <alanvardy@gmail.com>
2019-10-03 15:43:39 -0600

    formatting

--- lib/exzeitable.ex
+++ lib/exzeitable.ex
@@ -1,7 +1,7 @@
 defmodule Exzeitable do
   @moduledoc """
   # Exzeitable
-  
+
   Dynamic searchable, sortable datatable that takes a database query and a module, and makes the magic happen. Uses Phoenix Liveview and Postgres. Bootstrap friendly and easily configured for other CSS frameworks.
 
   ![Exzeitable](assets/screenshot.png)
