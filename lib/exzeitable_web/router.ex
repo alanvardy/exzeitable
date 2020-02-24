c03bf792cb76f2d1e371bd38dbc83428a8b83c56
Alan Vardy <alanvardy@gmail.com>
2020-02-24 06:54:53 -0800

    fix flash

--- lib/exzeitable_web/router.ex
+++ lib/exzeitable_web/router.ex
@@ -5,8 +5,7 @@ defmodule TestWeb.Router do
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
-    plug(:fetch_flash)
-    plug(Phoenix.LiveView.Flash)
+    plug(:fetch_live_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
   end
