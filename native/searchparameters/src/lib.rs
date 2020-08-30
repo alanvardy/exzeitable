bef5b4ae48a562740f84bc93a957b39a6a8b0782
Alan Vardy <alanvardy@gmail.com>
2020-08-30 16:46:02 -0700

    Working rust version

--- native/searchparameters/src/lib.rs
+++ native/searchparameters/src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rustler;
 
+use regex::Regex;
 use rustler::{Encoder, Env, Error, Term};
 
 mod atoms {
@@ -23,5 +24,35 @@ rustler::rustler_export_nifs! {
 fn convert<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     let string: String = args[0].decode()?;
 
-    Ok((atoms::ok(), string).encode(env))
+    let word_characters = remove_special_characters(&string);
+    let result = build_query(&word_characters);
+
+    Ok((atoms::ok(), result).encode(env))
+}
+
+fn remove_special_characters(s: &str) -> String {
+    let non_word_characters = Regex::new(r"[^A-Za-z0-9\s]").unwrap();
+
+    non_word_characters.replace_all(s, "").to_string()
+}
+
+fn build_query(s: &str) -> String {
+    let string_list: Vec<&str> = s.split_whitespace().collect();
+    let mut counter = 0;
+    let length = string_list.len();
+    let mut result = String::new();
+
+    loop {
+        if counter < length - 1 {
+            result.push_str(string_list[counter]);
+            result.push_str(":* & ");
+            counter += 1;
+        } else {
+            result.push_str(string_list[counter]);
+            result.push_str(":*");
+            break;
+        }
+    }
+
+    result
 }
