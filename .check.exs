c7e42ce008723c1f3f5aedef42a67200b3b30585
Alan Vardy <alanvardy@gmail.com>
2021-08-21 08:51:21 -0700

    chore: update CI
    add new setup config
    
    update other files
    
    wip
    
    fix command
    
    mergeme

--- .check.exs
+++ .check.exs
@@ -1,33 +1,13 @@
 [
-  ## all available options with default values (see `mix check` docs for description)
-  # skipped: true,
-  # exit_status: true,
-  # parallel: true,
-
-  ## list of tools (see `mix check` docs for defaults)
   retry: false,
   tools: [
-    ## curated tools may be disabled (e.g. the check for compilation warnings)
-    # {:compiler, false},
-
-    ## ...or adjusted (e.g. use one-line formatter for more compact credo output)
-    # {:credo, command: "mix credo --format oneline"},
-
-    ## custom new tools may be added (mix tasks or arbitrary commands)
-    # {:my_mix_check, command: "mix release", env: %{"MIX_ENV" => "prod"}},
-    # {:my_arbitrary_check, command: "npm test", cd: "assets"},
-
-    {:cypress, command: "mix cypress.run", deps: [{:credo, status: :ok}]},
-    {:dialyzer, deps: [{:credo, status: :ok}]},
-    {:ex_doc, deps: [{:credo, status: :ok}]},
+    {:cypress, command: "mix cypress.run"},
     {:npm_test, false},
     {:ex_coveralls,
      command: "mix coveralls.html",
      require_files: ["test/test_helper.exs"],
-     env: %{"MIX_ENV" => "test"}, deps: [{:credo, status: :ok}]},
+     env: %{"MIX_ENV" => "test"}},
     {:credo, command: "mix credo --strict"},
-    {:ex_unit, command: "mix test", env: %{"MIX_ENV" => "test"}, deps: [{:credo, status: :ok}]}
-
-    # {:my_arbitrary_script, command: ["my_script", "argument with spaces"], cd: "scripts"}
+    {:ex_unit, command: "mix test", env: %{"MIX_ENV" => "test"}}
   ]
 ]
