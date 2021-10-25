67d7dd917689d579ae4df0e19767410b0f9b11c6
Alan Vardy <alanvardy@gmail.com>
2021-10-25 06:46:47 -0700

    chore: use npx to run cypress

--- cypress-open.sh
+++ cypress-open.sh
@@ -7,7 +7,7 @@ echo "===RUN npm install cypress --save-dev ==="
 echo "===IN THE assets/ FOLDER==="
 MIX_ENV=systemtest mix phx.server &
 pid=$! # Store server pid
-./assets/node_modules/.bin/cypress open
+npx cypress open
 result=$?
 kill -9 $pid # Kill server
 echo "===KILLING PHX SERVER==="
--- cypress-run.sh
+++ cypress-run.sh
@@ -7,7 +7,7 @@ echo "===RUN npm install cypress --save-dev ==="
 echo "===IN THE assets/ FOLDER==="
 MIX_ENV=systemtest mix phx.server &
 pid=$! # Store server pid
-./assets/node_modules/.bin/cypress run
+npx cypress run
 result=$?
 kill -9 $pid # kill server
 echo "===KILLING PHX SERVER==="
