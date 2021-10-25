a1edefb103be8def01b2ae390987bc6050b6f585
Alan Vardy <alanvardy@gmail.com>
2021-10-24 20:02:02 -0700

    chore: update ci

--- .check.exs
+++ .check.exs
@@ -9,7 +9,7 @@
     {:cypress, command: "mix cypress.run", deps: [{:docker_up, status: :ok}]},
     {:npm_test, false},
     {:ex_coveralls,
-     command: "mix coveralls.html",
+     command: "mix coveralls.lcov",
      require_files: ["test/test_helper.exs"],
      env: %{"MIX_ENV" => "test"},
      deps: [{:install_cypress, status: :ok}]},
