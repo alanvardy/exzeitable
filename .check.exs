0bc637e06875d55c590998dc06f475e3cdf8e658
Alan Vardy <alanvardy@gmail.com>
2021-10-25 06:46:47 -0700

    chore: use npx to run cypress

--- .check.exs
+++ .check.exs
@@ -2,16 +2,12 @@
   retry: false,
   tools: [
     {:deploy_assets, command: "mix assets.deploy"},
-    {:install_cypress,
-     command: "npm install cypress --save-dev --prefix assets",
-     deps: [{:deploy_assets, status: :ok}]},
-    {:cypress, command: "mix cypress.run"},
+    {:cypress, command: "mix cypress.run", deps: [{:deploy_assets, status: :ok}]},
     {:npm_test, false},
     {:ex_coveralls,
      command: "mix coveralls.lcov",
      require_files: ["test/test_helper.exs"],
-     env: %{"MIX_ENV" => "test"},
-     deps: [{:install_cypress, status: :ok}]},
+     env: %{"MIX_ENV" => "test"}},
     {:credo, command: "mix credo --strict"},
     {:ex_unit, enabled: false}
   ]
