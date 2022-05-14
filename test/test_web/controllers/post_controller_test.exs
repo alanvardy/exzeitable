d80bb1ce779f9f38290b955545fee6be75a3eae2
Alan Vardy <alan@alanvardy.com>
2022-05-13 19:49:33 -0700

    Add controller tests

--- test/test_web/controllers/post_controller_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule TestWeb.PageControllerTest do
-  use TestWeb.ConnCase, async: true
-
-  test "Get posts page", %{conn: conn} do
-    conn = get(conn, Routes.post_path(conn, :index))
-
-    assert html_response(conn, 200) =~ "Posts"
-  end
-end
