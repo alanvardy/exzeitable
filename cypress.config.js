29f3c2044e42d66b4df136ec20d7ab629777c434
Alan Vardy <alan@vardy.cc>
2022-12-20 10:11:00 -0800

    Update Cypress specs

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
+    testIsolation: false,
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
--- cypress/e2e/acceptance.cy.js
+++ cypress/e2e/acceptance.cy.js
@@ -1,15 +1,4 @@
 describe('Acceptance Test', function () {
-  beforeEach(function () {
-    // before each test, we can automatically preserve the
-    // 'session_id' and 'remember_token' cookies. this means they
-    // will not be cleared before the NEXT test starts.
-    //
-    // the name of your cookies will likely be different
-    // this is just a simple example
-    Cypress.Cookies.debug(true)
-    Cypress.Cookies.preserveOnce('_tradesway_key')
-  })
-
   // REGULAR ROUTE
 
   it('Can visit the users page', function () {
